179d199e8a607565bab593f195df2374f954376a
Marko Maksimovic <kmeekmeekmee@gmail.com>
2025-12-05 15:18:45 +0100

    Add CRD (no U :O) for cash registers.

--- Projektovanje/backend/efikas/src/main/java/org/unibl/etf/efikas/models/responses/CashRegisterResponse.java
+++ Projektovanje/backend/efikas/src/main/java/org/unibl/etf/efikas/models/responses/CashRegisterResponse.java
@@ -4,6 +4,7 @@ import lombok.Data;
 
 @Data
 public class CashRegisterResponse {
+    private Integer cashRegisterId;
     private Integer cashRegisterNumber;
     private String softwareVersion;
 }
