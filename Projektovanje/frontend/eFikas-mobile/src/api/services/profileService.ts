d10196175e9ee8f593b0556f34af3d6edb538b2f
Andjela Balaban <balaban.andjela2@gmail.com>
2025-12-07 18:52:28 +0100

    Integrate backend with frontend for profile and cash register features

--- Projektovanje/frontend/eFikas-mobile/src/api/services/profileService.ts
+++ Projektovanje/frontend/eFikas-mobile/src/api/services/profileService.ts
@@ -2,37 +2,13 @@ import axiosInstance from "../axiosInstance";
 import { API_URLS } from "@/src/util/apiConstants";
 import { ProfileData } from "@/src/types/types";
 
-// export const profileService = {
-//     fetchProfile: async (): Promise<ProfileData> => {
-//         const response = await axiosInstance.get<ProfileData>(API_URLS.profile.get);
-//         return response.data;
-//     },
-
-//     updateProfile: async (data: ProfileData): Promise<void> => {
-//         await axiosInstance.put(API_URLS.profile.update, data);
-//     }
-// };
-
-
-// MOCK podaci
-const mockProfile: ProfileData = {
-    firstName: "Marko",
-    lastName: "Markovic",
-    taxId: "1234567890",
-    registerNumber: "54321",
-    softwareVersion: "2.1.0",
-};
-
 export const profileService = {
     fetchProfile: async (): Promise<ProfileData> => {
-        // simuliramo network delay
-        return new Promise(resolve => setTimeout(() => resolve(mockProfile), 500));
+        const response = await axiosInstance.get<ProfileData>(API_URLS.profile.get);
+        return response.data;
     },
 
     updateProfile: async (data: ProfileData): Promise<void> => {
-        return new Promise(resolve => setTimeout(() => {
-            console.log("Profil sacuvan:", data);
-            resolve();
-        }, 500));
-    },
+        await axiosInstance.put(API_URLS.profile.update, data);
+    }
 };
