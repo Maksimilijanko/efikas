b6746eacdb55b7bcbe92efad6cd08bb60dbd7646
Ivan Kuruzovic <ivan.kuruzovic@student.etf.unibl.org>
2025-12-07 11:46:35 +0100

    Removed comment from apiConstants.ts

--- Projektovanje/frontend/eFikas-mobile/src/util/apiConstants.ts
+++ Projektovanje/frontend/eFikas-mobile/src/util/apiConstants.ts
@@ -1,5 +1,5 @@
 const scheme = "https"
-const address = "efikas-team.com";    // TODO: Naći način da se gađa jedinstveni backend. Expo ne dozvoljava localhost izvršavanje jer to gađa sam uređaj - mora LAN za sad!
+const address = "efikas-team.com";   
 const port = "443";
 const version = "v1";
 export const API_BASE_URL = `${scheme}://${address}:${port}/api/${version}`;
