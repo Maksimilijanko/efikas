8e56d9a291bea21848a0e94fa4fc4a2d0024b3d5
Marko Maksimovic <kmeekmeekmee@gmail.com>
2025-12-24 00:52:31 +0100

    Add GET endpoint for obtaining all possible apartment traits. Add ApartmentTrait enum type and response.

---
+++ Projektovanje/backend/efikas/src/main/java/org/unibl/etf/efikas/models/responses/ApartmentTraitResponse.java
@@ -0,0 +1,10 @@
+package org.unibl.etf.efikas.models.responses;
+
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@RequiredArgsConstructor
+public class ApartmentTraitResponse {
+    private String keyName;
+}
