a22e66ecacd7dfc07ea2f70d88bd2887fbf3e46d
Ivan Kuruzovic <ivan.kuruzovic@student.etf.unibl.org>
2026-01-01 22:30:12 +0100

    .

--- Projektovanje/backend/efikas/src/main/java/org/unibl/etf/efikas/repositories/GuestsBookRepository.java
+++ Projektovanje/backend/efikas/src/main/java/org/unibl/etf/efikas/repositories/GuestsBookRepository.java
@@ -4,7 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.unibl.etf.efikas.models.entities.GuestsBook;
 
+import java.util.Optional;
+
 public interface GuestsBookRepository extends
         JpaRepository<GuestsBook, Integer>,
         JpaSpecificationExecutor<GuestsBook> {
+    Optional<GuestsBook> findByCitizenId(String citizenId);
 }
