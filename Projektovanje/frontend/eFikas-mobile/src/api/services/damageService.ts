f7c001737464c86cbf44ea757e23438416fe2d62
Sonja03 <sonja.galic@student.etf.unibl.org>
2026-02-01 19:40:56 +0100

    Resolved a bug in taskService, damageService, and expenseService that prevented the status update of finished items.

--- Projektovanje/frontend/eFikas-mobile/src/api/services/damageService.ts
+++ Projektovanje/frontend/eFikas-mobile/src/api/services/damageService.ts
@@ -12,12 +12,33 @@ export const damageService = {
         return response.data;
     },
 
-    updateStatus: async (apartmentId: number, name: string, currentData: ApartmentDamageDTO) => {
-        const payload = { ...currentData, status: true }; 
-        const response = await axiosInstance.put(
-            `/apartments/${apartmentId}/damages/${encodeURIComponent(name)}`, 
-            payload
-        );
-        return response.data;
-    }
+     updateStatus: async (apartmentId: number, name: string, currentData: ApartmentDamageDTO) => {
+
+    // Ovako filtriramo samo polja koja Java DTO dozvoljava
+
+    const payload = {
+
+        name: currentData.name,
+
+        damagePrice: currentData.damagePrice,
+
+        note: currentData.note,
+
+        status: true // Ovde ga menjamo
+
+    };
+
+
+
+    const response = await axiosInstance.put(
+
+        `/apartments/${apartmentId}/damages/${encodeURIComponent(name)}`,
+
+        payload
+
+    );
+
+    return response.data;
+
+}
 };
